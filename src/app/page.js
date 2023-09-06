ac469551ccb8bcb23b257e7a2d45961c4ce7b3bf
Rick Vermeil <rickv85@gmail.com>
2023-09-06 11:15:25 -0400

    Build initial mobile nav layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,16 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
-        <div className={styles.gallery} >
-          <a className={styles.link} href="/architecture">
-            TEST
-          </a>
+        <div className={styles.gallery}>
+          {/* Make this nav a component to reuse on all pages */}
+          <nav className={styles["nav-background"]}>
+            <h1 className={styles["site-title"]}>David M. Budd Photography</h1>
+            <div className={styles["hamburger-menu"]}>
+              <div className={styles.line1}></div>
+              <div className={styles.line2}></div>
+              <div className={styles.line3}></div>
+            </div>
+          </nav>
           <Image
             src="/images/DMB_9836.jpg"
             layout="fill"
