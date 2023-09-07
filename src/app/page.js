959a8f1cef360d3f7617122b7b6e2ff24781eac2
Rick Vermeil <rickv85@gmail.com>
2023-09-07 08:21:11 -0400

    Add additional divs

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -16,47 +16,60 @@ export default function Home() {
   useEffect(() => {
     updateDimensions();
 
-    window.addEventListener('resize', updateDimensions);
+    window.addEventListener("resize", updateDimensions);
 
     return () => {
-      window.removeEventListener('resize', updateDimensions);
+      window.removeEventListener("resize", updateDimensions);
     };
   }, []);
 
   return (
-    <main
-      className={styles.main}
+    <div
       style={{
         width: `${dimensions.width}px`,
         height: `${dimensions.height}px`,
+        position: "relative",
       }}
     >
-      <div className={styles.layout}>
-        {/* Make this nav a component to reuse on all pages */}
-        <nav className={styles["nav-background"]}>
-          <h1 className={styles["site-title"]}>David M. Budd Photography</h1>
-          <div className={styles["hamburger-menu"]}>
-            <div className={styles.line1}></div>
-            <div className={styles.line2}></div>
-            <div className={styles.line3}></div>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          overflow: "hidden",
+        }}
+      >
+        <main className={styles.main}>
+          <div className={styles.layout}>
+            {/* Make this nav a component to reuse on all pages */}
+            <nav className={styles["nav-background"]}>
+              <h1 className={styles["site-title"]}>
+                David M. Budd Photography
+              </h1>
+              <div className={styles["hamburger-menu"]}>
+                <div className={styles.line1}></div>
+                <div className={styles.line2}></div>
+                <div className={styles.line3}></div>
+              </div>
+            </nav>
+            <Image
+              className={styles.gallery}
+              src="/images/DMB_9836.jpg"
+              width={3600}
+              height={2400}
+              quality={100}
+              priority={true}
+              alt={`image gallery of David's work`}
+            />
+            {/* Make this footer a component, same as nav */}
+            <footer className={styles["footer-background"]}>
+              <h3>Based in Denver, Colorado</h3>
+              <h3>{`(303) 807-8479`}</h3>
+              <h3>dbuddphoto@me.com</h3>
+            </footer>
           </div>
-        </nav>
-        <Image
-          className={styles.gallery}
-          src="/images/DMB_9836.jpg"
-          width={3600}
-          height={2400}
-          quality={100}
-          priority={true}
-          alt={`image gallery of David's work`}
-        />
-        {/* Make this footer a component, same as nav */}
-        <footer className={styles["footer-background"]}>
-          <h3>Based in Denver, Colorado</h3>
-          <h3>{`(303) 807-8479`}</h3>
-          <h3>dbuddphoto@me.com</h3>
-        </footer>
+        </main>
       </div>
-    </main>
+    </div>
   );
 }
