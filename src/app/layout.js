f03a205305878b398e0211acc16c1c2e25acac45
Rick Vermeil <rickv85@gmail.com>
2023-09-07 10:29:10 -0400

    Add manifest and Head to layout for testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'David M. Budd Photography',
-  description: 'Photographer for hire in Denver, CO',
-}
+  title: "David M. Budd Photography",
+  description: "Photographer for hire in Denver, CO",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
