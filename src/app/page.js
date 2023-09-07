394c8bd74108651900c373d0e65238490fc87d85
Rick Vermeil <rickv85@gmail.com>
2023-09-07 08:38:17 -0400

    Add --vh style property

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,24 @@
-// "use client";
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
-// import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  // const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  useEffect(() => {
+    const updateViewHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    
+    updateViewHeight();
 
-  // const updateDimensions = () => {
-  //   const width = window.innerWidth;
-  //   const height = window.innerHeight;
-  //   setDimensions({ width, height });
-  // };
+    window.addEventListener('resize', updateViewHeight);
 
-  // useEffect(() => {
-  //   updateDimensions();
-
-  //   window.addEventListener("resize", updateDimensions);
-
-  //   return () => {
-  //     window.removeEventListener("resize", updateDimensions);
-  //   };
-  // }, []);
+    return () => window.removeEventListener('resize', updateViewHeight);
+  }, []);
 
   return (
-    // <div
-    //   style={{
-    //     width: `${dimensions.width}px`,
-    //     height: `${dimensions.height}px`,
-    //     position: "relative",
-    //   }}
-    // >
-    //   <div
-    //     style={{
-    //       width: "100%",
-    //       height: "100%",
-    //       position: "absolute",
-    //       overflow: "hidden",
-    //     }}
-    //   >
         <main className={styles.main}>
           <div className={styles.layout}>
             {/* Make this nav a component to reuse on all pages */}
@@ -69,7 +49,5 @@ export default function Home() {
             </footer>
           </div>
         </main>
-    //   </div>
-    // </div>
   );
 }
