e70a8673e4480a462796ae000fdbcbcf84b53440
Rick Vermeil <rickv85@gmail.com>
2023-09-08 07:36:50 -0400

    Remove Head, add to metadata obj

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,18 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "David M. Budd Photography",
   description: "Photographer for hire in Denver, CO",
+  themeColor: "#272727",
+  backgroundColor: "#272727"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="theme-color" content="#272727"></meta>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
