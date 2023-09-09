02a9774eb2fa3b984e22e30f70cd63bc06290023
Rick Vermeil <rickv85@gmail.com>
2023-09-09 09:47:59 -0400

    Adjust image sizing, install Cloudinary CDN but not yet using

--- src/app/components/HomeSwiper/HomeSwiper.jsx
+++ src/app/components/HomeSwiper/HomeSwiper.jsx
@@ -6,6 +6,12 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "./HomeSwiper.css";
 
+// Installed Cloudinary - if not using, run "npm uninstall @cloudinary/url-gen @cloudinary/react",
+// and remove from next.config.js
+// import {Cloudinary} from "@cloudinary/url-gen";
+
+// const cld = new Cloudinary({cloud: {cloudName: 'ddsopsgpi'}});
+
 export default function HomeSwiper() {
   return (
     <Swiper
@@ -23,7 +29,7 @@ export default function HomeSwiper() {
         <Image width={810} height={1440} priority="true" src="/images/vertical/florissanthornbek_916.webp" alt="starry night sky" />
       </SwiperSlide>
       <SwiperSlide>
-        <Image width={3375} height={6000} priority="true" src="/images/vertical/christy_bcpoint1_916.webp" alt="patio with mountain view" />
+        <Image width={810} height={1440} priority="true" src="/images/vertical/christy_bcpoint1_916.webp" alt="patio with mountain view" />
       </SwiperSlide>
       <SwiperSlide>
         <Image width={810} height={1440} loading="lazy" src="/images/vertical/4014_916.webp" alt="steam train engine in field" />
@@ -32,7 +38,7 @@ export default function HomeSwiper() {
         <Image width={810} height={1440} loading="lazy" src="/images/vertical/kotor 916.webp" alt="boat on waterway in front of a mountain" />
       </SwiperSlide>
       <SwiperSlide>
-        <Image width={809} height={1439} loading="lazy" src="/images/vertical/Indyplaza 916.webp" alt="elevator shaft in an elegant building" />
+        <Image width={810} height={1440} loading="lazy" src="/images/vertical/Indyplaza 916.webp" alt="elevator shaft in an elegant building" />
       </SwiperSlide>
       <SwiperSlide>
         <Image width={810} height={1440} loading="lazy" src="/images/vertical/edison7tvfinal_916.webp" alt="living room" />
