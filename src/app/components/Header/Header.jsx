8f7a267c16474ed161b6f2ba3d098e9d07e14ca4
Rick Vermeil <rickv85@gmail.com>
2023-09-11 14:08:59 -0600

    Add Hamburger, and open dropdown menu

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,14 +1,36 @@
-import styles from "./Header.module.css"
+"use client";
+import { useState } from "react";
+import styles from "./Header.module.css";
+import { Squash as Hamburger } from "hamburger-react";
 
 export default function Header() {
+  const [hamMenuOpen, setHamMenuOpen] = useState(false);
+
+  const toggleHamMenu = () => {
+    setHamMenuOpen(!hamMenuOpen);
+  };
+
   return (
-    <nav className={styles["nav-background"]}>
-      <h1 className={styles["site-title"]}>David M. Budd Photography</h1>
-      <div className={styles["hamburger-menu"]}>
-        <div className={styles.line1}></div>
-        <div className={styles.line2}></div>
-        <div className={styles.line3}></div>
+    <nav className={styles["nav-main"]}>
+      <div className={styles["nav-background"]}>
+        <h1 className={styles["site-title"]}>David M. Budd Photography</h1>
+        <div className={styles["hamburger-menu"]}>
+          <Hamburger
+            color="#ffffff"
+            toggled={hamMenuOpen}
+            toggle={toggleHamMenu}
+          />
+        </div>
       </div>
+      <menu className={styles["dropdown-menu"]}>
+        <line className={styles["dropdown-divider"]}></line>
+        <h2 className={styles["menu-option"]}>Architecture</h2>
+        <h2 className={styles["menu-option"]}>Product</h2>
+        <h2 className={styles["menu-option"]}>Landscape</h2>
+        <h2 className={styles["menu-option"]}>Lifestyle</h2>
+        <h2 className={styles["menu-option"]}>About/Contact</h2>
+        <line className={styles["dropdown-divider"]}></line>
+      </menu>
     </nav>
   );
 }
