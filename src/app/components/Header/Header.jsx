758958b4e5ce7063e7e35a91c6732b01a02dfdcd
Rick Vermeil <rickv85@gmail.com>
2023-09-11 17:50:57 -0600

    Add initial style changes for header, justify header, add new class for desktop-site-title

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -6,6 +6,9 @@ import { Squash as Hamburger } from "hamburger-react";
 
 export default function Header() {
   const [hamMenuOpen, setHamMenuOpen] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(undefined);
+  const [screenHeight, setScreenHeight] = useState(undefined);
+  const [headerDisplayMode, setHeaderDisplayMode] = useState(undefined);
 
   const toggleHamMenu = useCallback(() => {
     setHamMenuOpen((prevHamMenuOpen) => !prevHamMenuOpen);
@@ -30,19 +33,52 @@ export default function Header() {
     };
   }, [hamMenuOpen, handleClickOutside]);
 
+  useEffect(() => {
+    const updateViewport = () => {
+      const vw = window.innerWidth;
+      const vh = window.innerHeight;
+
+      setScreenWidth(vw);
+      setScreenHeight(vh);
+    };
+
+    updateViewport();
+
+    window.addEventListener("resize", updateViewport);
+    window.addEventListener("orientationchange", updateViewport);
+
+    return () => {
+      window.removeEventListener("resize", updateViewport);
+      window.removeEventListener("orientationchange", updateViewport);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (screenWidth > 1024) {
+      setHeaderDisplayMode("desktop");
+      document.documentElement.style.setProperty("--headerFlexJustify", 'center');
+    } else {
+      setHeaderDisplayMode("mobile");
+      document.documentElement.style.setProperty("--headerFlexJustify", 'space-between');
+    }
+  }, [screenWidth]);
+
   return (
     <nav className={styles["nav-main"]}>
       <div className={styles["nav-background"]}>
         <a href="/">
-          <h1 className={styles["site-title"]}>David M. Budd Photography</h1>
+          <h1 className={headerDisplayMode === "mobile" ? styles["mobile-site-title"] : styles["desktop-site-title"]}>David M. Budd Photography</h1>
         </a>
-        <div className={styles["hamburger-menu"]} aria-label="Hamburger menu button">
-          <Hamburger
-            color="#ffffff"
-            toggled={hamMenuOpen}
-            toggle={toggleHamMenu}
-          />
-        </div>
+        {headerDisplayMode === "mobile" ? (
+          <div className={styles["hamburger-menu"]}>
+            <Hamburger
+              color="#ffffff"
+              toggled={hamMenuOpen}
+              toggle={toggleHamMenu}
+              label="show menu items"
+            />
+          </div>
+        ) : null}
       </div>
       <menu
         id="dropdownMenu"
