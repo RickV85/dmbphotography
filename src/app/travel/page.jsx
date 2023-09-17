fc9e363eb7b108f1917495d89b114b28a97343b9
Rick Vermeil <rickv85@gmail.com>
2023-09-17 07:59:14 -0600

    Apply prev commit changes to Travel page

--- src/app/travel/page.jsx
+++ src/app/travel/page.jsx
@@ -8,12 +8,14 @@ import Image from "next/image";
 export default function Travel() {
   return (
     <main className={styles.main}>
-      <Header sectionTitle="Travel"/>
+      <Header sectionTitle="Travel" />
       <div className={styles["rotate-message-container"]}>
-        <h3 className={styles["rotate-message"]}>Best viewed in landscape - Rotate device</h3>
+        <h3 className={styles["rotate-message"]}>
+          Best viewed in landscape - Rotate device
+        </h3>
       </div>
       <div className={styles["background-image-container"]}>
-      {/* might need to adjust these fixed vertical dims for bkgd img */}
+        {/* might need to adjust these fixed vertical dims for bkgd img */}
         <Image
           priority={true}
           src={"/images/home/horizontal/florissanthornbek_final webhor.webp"}
@@ -23,10 +25,17 @@ export default function Travel() {
           className={styles["gallery-background-img"]}
         />
       </div>
-      <GallerySwiper images={{horiz: travelHorizImages, vert: travelVertImages}} />
-      <div className={styles["section-title-container"]}>
-        <h2>Travel</h2>
-      </div>
+      <section className={styles.gallery}>
+        <div className={styles["gallery-spacer"]}></div>
+        <div className={styles["gallery-swiper"]}>
+          <GallerySwiper
+            images={{ horiz: travelHorizImages, vert: travelVertImages }}
+          />
+        </div>
+        <div className={styles["section-title-container"]}>
+          <h2>Travel</h2>
+        </div>
+      </section>
     </main>
   );
 }
