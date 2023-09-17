612abd4e345370ca8ab29975f92c356d29d70f83
Rick Vermeil <rickv85@gmail.com>
2023-09-17 07:54:02 -0600

    Fix mobile vert layout issues for responsiveness

--- src/app/architecture/page.jsx
+++ src/app/architecture/page.jsx
@@ -8,25 +8,32 @@ import Image from "next/image";
 export default function Architecture() {
   return (
     <main className={styles.main}>
-      <Header sectionTitle="Architecture"/>
-      <div className={styles["rotate-message-container"]}>
-        <h3>Best viewed in landscape - Rotate device</h3>
-      </div>
-      <div className={styles["background-image-container"]}>
-      {/* might need to adjust these fixed vertical dims for bkgd img */}
-        <Image
-          priority={true}
-          src={"/images/home/vertical/Indyplaza 916.webp"}
-          alt={"elevator shaft in an elegant building"}
-          height={1440}
-          width={810}
-          className={styles["gallery-background-img"]}
-        />
-      </div>
-      <GallerySwiper images={{horiz: archHorizImages, vert: archVertImages}} />
-      <div className={styles["section-title-container"]}>
-        <h2>Architecture</h2>
-      </div>
+      <Header sectionTitle="Architecture" />
+        <div className={styles["rotate-message-container"]}>
+          <h3>Best viewed in landscape - Rotate device</h3>
+        </div>
+        <div className={styles["background-image-container"]}>
+          {/* might need to adjust these fixed vertical dims for bkgd img */}
+          <Image
+            priority={true}
+            src={"/images/home/vertical/Indyplaza 916.webp"}
+            alt={"elevator shaft in an elegant building"}
+            height={1440}
+            width={810}
+            className={styles["gallery-background-img"]}
+          />
+        </div>
+        <section className={styles.gallery}>
+        <div className={styles["gallery-spacer"]}></div>
+        <div className={styles["gallery-swiper"]}>
+          <GallerySwiper
+            images={{ horiz: archHorizImages, vert: archVertImages }}
+          />
+        </div>
+        <div className={styles["section-title-container"]}>
+          <h2>Architecture</h2>
+        </div>
+      </section>
     </main>
   );
 }
--- src/app/components/HomeSwiper/loading.js
+++
@@ -1,9 +0,0 @@
-
-// Need to figure out how this is activated in Next13
-export default function Loading() {
-  return (
-    <div style={{ backgroundColor: "red" }}>
-      <h2>LOADING</h2>
-    </div>
-  );
-}
