2bcd381fbd86b141e8328a8e4b373eefdf0d2719
Rick Vermeil <rickv85@gmail.com>
2023-09-23 09:04:37 -0600

    Create Gallery component, successful test on Architecture

---
+++ src/app/components/Gallery/Gallery.jsx
@@ -0,0 +1,48 @@
+import styles from "./gallery.module.css";
+import Header from "../../components/Header/Header";
+import GallerySwiper from "../../components/GallerySwiper/GallerySwiper";
+import Image from "next/image";
+
+// Props needed to make each unique:
+// sectionTitle
+// backgroundImgSrc
+// backgroundImgAltText
+// backgroundImgDims - Obj w/ width x height
+// gallerySwiperImgs - Obj
+//    horiz: src path, vert: src path
+
+export default function Gallery({ galleryProps }) {
+  return (
+    <>
+      <Header sectionTitle={galleryProps.sectionTitle} />
+      <div className={styles["rotate-message-container"]}>
+        <h3>Best viewed in landscape - Rotate device</h3>
+      </div>
+      <div className={styles["background-image-container"]}>
+        {/* might need to adjust these fixed vertical dims for bkgd img */}
+        <Image
+          priority={true}
+          src={galleryProps.backgroundImgSrc}
+          alt={galleryProps.backgroundImgAltText}
+          height={galleryProps.backgroundImgDims.height}
+          width={galleryProps.backgroundImgDims.width}
+          className={styles["gallery-background-img"]}
+        />
+      </div>
+      <section className={styles.gallery}>
+        <div className={styles["gallery-spacer"]}></div>
+        <div className={styles["gallery-swiper"]}>
+          <GallerySwiper
+            images={{
+              horiz: galleryProps.gallerySwiperImgs.horiz,
+              vert: galleryProps.gallerySwiperImgs.vert,
+            }}
+          />
+        </div>
+        <div className={styles["section-title-container"]}>
+          <h2>{galleryProps.sectionTitle}</h2>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/galleries/architecture/page.jsx
+++ src/app/galleries/architecture/page.jsx
@@ -1,39 +1,20 @@
-import styles from "./page.module.css";
-import Header from "../../components/Header/Header";
-import GallerySwiper from "../../components/GallerySwiper/GallerySwiper";
+// import styles from "./page.module.css";
+// import Header from "../../components/Header/Header";
+// import GallerySwiper from "../../components/GallerySwiper/GallerySwiper";
+import Gallery from "../../components/Gallery/Gallery";
 import archHorizImages from "./ArchitectureHorizImages";
 import archVertImages from "./ArchitectureVertImages";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Architecture() {
+  const architectureProps = {
+    sectionTitle: "Architecture",
+    backgroundImgSrc: archVertImages[1].src,
+    backgroundImgAltText: archVertImages[1].alt,
+    backgroundImgDims: {width: 810, height: 1440},
+    gallerySwiperImgs: {horiz: archHorizImages, vert: archVertImages}
+  }
   return (
-    <>
-      <Header sectionTitle="Architecture" />
-      <div className={styles["rotate-message-container"]}>
-        <h3>Best viewed in landscape - Rotate device</h3>
-      </div>
-      <div className={styles["background-image-container"]}>
-        {/* might need to adjust these fixed vertical dims for bkgd img */}
-        <Image
-          priority={true}
-          src={"/images/home/vertical/Indyplaza 916.webp"}
-          alt={"elevator shaft in an elegant building"}
-          height={1440}
-          width={810}
-          className={styles["gallery-background-img"]}
-        />
-      </div>
-      <section className={styles.gallery}>
-        <div className={styles["gallery-spacer"]}></div>
-        <div className={styles["gallery-swiper"]}>
-          <GallerySwiper
-            images={{ horiz: archHorizImages, vert: archVertImages }}
-          />
-        </div>
-        <div className={styles["section-title-container"]}>
-          <h2>Architecture</h2>
-        </div>
-      </section>
-    </>
+    <Gallery galleryProps={architectureProps} />
   );
 }
