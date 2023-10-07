a78f0ab3e136e4b57809d94f6987b91e28a1348f
Rick Vermeil <rickv85@gmail.com>
2023-10-07 16:42:43 -0600

    Disable loading component, functions and load all at high priority

--- src/app/components/GallerySwiper/GallerySwiper.jsx
+++ src/app/components/GallerySwiper/GallerySwiper.jsx
@@ -1,27 +1,30 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
-import "./GallerySwiper.css"
+import { useState, useEffect, useRef } from "react";
+import "./GallerySwiper.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import Loading from "./Loading";
 
 export default function GallerySwiper({ images }) {
-  const [ galleryImages, setGalleryImages ] = useState(undefined);
-  const [ mobileRes, setMobileRes ] = useState(true);
+  const [galleryImages, setGalleryImages] = useState(undefined);
+  const [mobileRes, setMobileRes] = useState(true);
+  const loadedImgKeys = useRef([]);
+  // const isInitialImgLoaded = useRef(false);
 
   useEffect(() => {
     const updateViewport = () => {
       if (window.innerWidth > 550) {
-        console.log("Desktop resolution")
-        setMobileRes(false)
-      } else {
-        console.log("Mobile resolution")
-        setMobileRes(true)
+        console.log("Desktop resolution");
+        setMobileRes(false);
+      } else if (window.innerWidth < 550 && window.screen.orientation.type === "portrait-primary") {
+        console.log("Mobile resolution");
+        setMobileRes(true);
       }
 
       const vw = window.innerWidth * 0.01;
@@ -48,37 +51,50 @@ export default function GallerySwiper({ images }) {
     };
   }, [images]);
 
+  // useEffect(() => {
+  //   if (loadedImgKeys.current.length && loadedImgKeys.current.includes(0)) {
+  //     isInitialImageLoaded.current = true;
+  //     console.log("true");
+  //   }
+  // }, [galleryImages, loadedImgKeys]);
+
   return (
-    <Swiper
-      modules={[Navigation, Autoplay, Pagination]}
-      className="mySwiper"
-      navigation={true}
-      pagination={true}
-      spaceBetween={50}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: true,
-      }}
-      loop={true}
-      onSwiper={(swiper) => {
-        console.log(swiper);
-      }}
-    >
-      {galleryImages ? (
-        galleryImages.map((img, i) => (
-          <SwiperSlide key={i}>
-            <Image
-              fill={true}
-              priority={i <= 1 ? true : false}
-              quality={mobileRes ? 25 : 75}
-              src={img.src}
-              alt={img.alt}
-            />
-          </SwiperSlide>
-        ))
-      ) : (
-        <></>
-      )}
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Navigation, Autoplay, Pagination]}
+        className="mySwiper"
+        navigation={true}
+        pagination={true}
+        spaceBetween={50}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+        }}
+        loop={true}
+        onSwiper={(swiper) => {
+          console.log(swiper);
+        }}
+      >
+        {galleryImages ? (
+          galleryImages.map((img, i) => (
+            <SwiperSlide key={i}>
+              <Image
+                fill={true}
+                priority={true}
+                quality={mobileRes ? 25 : 75}
+                src={img.src}
+                alt={img.alt}
+                // onLoadingComplete={() => {
+                //   loadedImgKeys.current.push(i);
+                //   console.log(loadedImgKeys.current);
+                // }}
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <></>
+        )}
+      </Swiper>
+    </>
   );
 }
