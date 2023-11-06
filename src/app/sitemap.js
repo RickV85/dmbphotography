aab0ca2cea30f56f9d17faf71c5c2f1dedfc50dd
Rick Vermeil <rickv85@gmail.com>
2023-11-06 14:39:39 -0700

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.davidmbuddphotography.com/',
+      lastModified: new Date(), 
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.davidmbuddphotography.com/galleries/architecture',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.davidmbuddphotography.com/galleries/product',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.davidmbuddphotography.com/galleries/travel',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.davidmbuddphotography.com/galleries/lifestyle',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.davidmbuddphotography.com/about_contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
