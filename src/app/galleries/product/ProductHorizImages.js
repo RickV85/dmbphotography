802490e291e5c84b49830222a08fe66c8b5d394c
Rick Vermeil <rickv85@gmail.com>
2025-03-19 07:27:49 -0600

    Add/delete files to swap pics, update src/alt, format

--- src/app/galleries/product/ProductHorizImages.js
+++ src/app/galleries/product/ProductHorizImages.js
@@ -1,44 +1,44 @@
 const productHorizImages = [
   {
-    src: "/images/product/horizontal/christy_bcpoint4.webp",
-    alt: "patio furniture with a mountain backdrop",
+    src: '/images/product/horizontal/christy_bcpoint4.webp',
+    alt: 'patio furniture with a mountain backdrop',
   },
   {
-    src: "/images/product/horizontal/campy.webp",
-    alt: "closeup of a campy bike cassette",
+    src: '/images/product/horizontal/campy.webp',
+    alt: 'closeup of a campy bike cassette',
   },
   {
-    src: "/images/product/horizontal/christy patio 1.webp",
-    alt: "patio furniture with a mountain backdrop",
+    src: '/images/product/horizontal/christy patio 1.webp',
+    alt: 'patio furniture with a mountain backdrop',
   },
   {
-    src: "/images/product/horizontal/Kieding_IP_30.webp",
-    alt: "furniture in a lobby area",
+    src: '/images/product/horizontal/Kieding_IP_30.webp',
+    alt: 'furniture in a lobby area',
   },
   {
-    src: "/images/product/horizontal/kiedingbuttons_96.webp",
-    alt: "closeup of sunglasses",
+    src: '/images/product/horizontal/bloody-mary.webp',
+    alt: 'closeup of a bloody mary cocktail',
   },
   {
-    src: "/images/product/horizontal/NES101221 _17.webp",
-    alt: "closeup of a cocktail",
+    src: '/images/product/horizontal/NES101221 _17.webp',
+    alt: 'closeup of a cocktail',
   },
   {
-    src: "/images/product/horizontal/pelle.webp",
-    alt: "closeup of a Pellegrino bottle label",
+    src: '/images/product/horizontal/pelle.webp',
+    alt: 'closeup of a Pellegrino bottle label',
   },
   {
-    src: "/images/product/horizontal/pen.webp",
-    alt: "closeup of a calligraphy pen",
+    src: '/images/product/horizontal/vapordist_peach.webp',
+    alt: 'closeup of a peach cocktail',
   },
   {
-    src: "/images/product/horizontal/perfume_bw.webp",
-    alt: "perfume bottle and Bluewater climbing equipment",
+    src: '/images/product/horizontal/perfume_bw.webp',
+    alt: 'perfume bottle and Bluewater climbing equipment',
   },
   {
-    src: "/images/product/horizontal/whiskeyblue.webp",
-    alt: "closeup of a matchbox",
+    src: '/images/product/horizontal/whiskeyblue.webp',
+    alt: 'closeup of a matchbox',
   },
-]
+];
 
 module.exports = productHorizImages;
