732aa219899f6a79d44409da6a8de66d95d5e046
Rick Vermeil <rickv85@gmail.com>
2025-03-19 07:00:03 -0600

    useRef instead of query select in GallerySwiper, change to client, formatting

--- src/app/components/Gallery/Gallery.jsx
+++ src/app/components/Gallery/Gallery.jsx
@@ -1,33 +1,35 @@
-import styles from "./gallery.module.css";
-import Header from "../../components/Header/Header";
-import GallerySwiper from "../../components/GallerySwiper/GallerySwiper";
-import BackgroundImg from "../BackgroundImg/BackgroundImg";
-import Footer from "../Footer/Footer";
+'use client';
+
+import { useRef } from 'react';
+
+import styles from './gallery.module.css';
+import Header from '../../components/Header/Header';
+import GallerySwiper from '../../components/GallerySwiper/GallerySwiper';
+import BackgroundImg from '../BackgroundImg/BackgroundImg';
+import Footer from '../Footer/Footer';
 
 export default function Gallery({ galleryProps }) {
+  const gallerySwiperDivRef = useRef(null);
+
   return (
     <>
       <Header sectionTitle={galleryProps.sectionTitle} />
-      <div className={styles["rotate-message-container"]}>
-        <span className={styles["rotate-message"]}>
-          Best viewed in landscape - Rotate device
-        </span>
+      <div className={styles['rotate-message-container']}>
+        <span className={styles['rotate-message']}>Best viewed in landscape - Rotate device</span>
       </div>
-      <BackgroundImg
-        vertImg={galleryProps.vertBackgroundImg}
-        horizImg={galleryProps.horizBackgroundImg}
-      />
+      <BackgroundImg vertImg={galleryProps.vertBackgroundImg} horizImg={galleryProps.horizBackgroundImg} />
       <section className={styles.gallery}>
-        <div className={styles["gallery-spacer"]}></div>
-        <div className={styles["gallery-swiper"]}>
+        <div className={styles['gallery-spacer']}></div>
+        <div className={styles['gallery-swiper']} ref={gallerySwiperDivRef}>
           <GallerySwiper
+            gallerySwiperDivRef={gallerySwiperDivRef}
             images={{
               horiz: galleryProps.gallerySwiperImgs.horiz,
               vert: galleryProps.gallerySwiperImgs.vert,
             }}
           />
         </div>
-        <div className={styles["section-title-container"]}>
+        <div className={styles['section-title-container']}>
           <h2>{galleryProps.sectionTitle}</h2>
         </div>
       </section>
